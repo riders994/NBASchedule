ec24bcc1d4f3ea7aece56d19d4192e56559c7646
Rohan Vahalia <r.vahalia@gmail.com>
2019-08-14 16:20:49 -0500

    ESPN switched from HTML to JS. Will have to rewrite in python temporarily.

--- conflict_score_generator.R
+++ conflict_score_generator.R
@@ -1,43 +1,60 @@
 library(XML)
+
 #Setting global vars and results
 teamAbrevs = c("ATL","BOS","BKN","CHA","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NOR","NYK","OKC","ORL","PHI","PHO","POR","SAC","SAS","TOR","UTH","WAS")
 dfList = list()
 masterDates = c()
-for(team in teamAbrevs)
-{
+
+for(team in teamAbrevs){
+
   #1.0 Pulling the table
+
   tab = readHTMLTable(paste("http://www.espn.com/nba/team/schedule/_/name/", team, "/seasontype/2", sep = ""))
+  
   #1.1 Unlisting
+  
   tab = tab[[1]]
   year = rep("2018",82)
+  
   #2.0 Cleaning Table
   #2.1 Remove Columns, Rows
+  
   tab = tab[-grep("OPPONENT",tab$V2),]
   tab = tab[,c(2,1)]
+  
   #2.2 Rename Columns
+  
   names(tab)=c("Opponent","Day")
+  
   #2.3 Add in Year
+  
   year[(min(grep("Jan", tab$Day))):82] = "2019"
+  
   #2.4 Add in Real Dates
+  
   dates = as.Date(paste(tab$Day,year), format = "%a, %b %d %Y")
   tab$Date = dates
   row.names(tab)=c()
+  
   #3.0 Add in to Main Results
+  
   dfList[[(length(dfList)+1)]] = tab
   masterDates = c(masterDates, dates)
 }
+
 allDates = unique(masterDates)
-teamAbrevs[25] = "PDX"; teamAbrevs[29] = "UTA";
+teamAbrevs[29] = "UTA";
 names(dfList) = teamAbrevs
 span = length(allDates)
 
 
 #Set up Conflict DF
 #1.0 Set up receivers and vars
+
 Conflicts = as.data.frame(matrix(ncol=0,nrow=span))
 dates = as.character(allDates)
-for(i in 1:30)
-{
+
+for(i in 1:30){
   Conflicts = cbind(Conflicts, as.numeric(allDates %in% dfList[[(i)]][,3]))
 }
 names(Conflicts) = teamAbrevs
@@ -56,8 +73,8 @@ teamTotals = teamTotals-82
 teamMean = teamTotals/m
 teamMean
 resmat = matrix(0,ncol=30,nrow=30)
-for(i in 1:29)
-{
+
+for(i in 1:29){
   dat = dfList[[(i)]][,3]
   for(j in (i+1):30)
   {
